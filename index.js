08a2b0bc1b87b69a8de5282bb1052fdc4bbd1c81
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-11-09 18:49:12 +0500

    Porting https://github.com/sindresorhus/got/pull/12

--- index.js
+++ index.js
@@ -6,33 +6,9 @@ var urlLib = require('url');
 var zlib = require('zlib');
 var PassThrough = require('stream').PassThrough;
 var assign = require('object-assign');
+var read = require('read-all-stream');
 
-function read(res, encoding, cb) {
-	if (!cb) {
-		cb = encoding;
-		encoding = undefined;
-	}
-
-	var chunks = [];
-	var len = 0;
-
-	res.on('data', function (chunk) {
-		chunks.push(chunk);
-		len += chunk.length;
-	});
-
-	res.once('end', function () {
-		var data = Buffer.concat(chunks, len);
-
-		if (encoding !== null) {
-			data = data.toString(encoding || 'utf8');
-		}
-
-		cb(null, data, res);
-	});
-}
-
-function sent(url, content, opts, cb) {
+module.exports = function (url, content, opts, cb) {
 	if (typeof opts === 'function') {
 		// If `cb` has been specified but `opts` has not.
 		cb = opts;
@@ -70,9 +46,12 @@ function sent(url, content, opts, cb) {
 		var fn = parsedUrl.protocol === 'https:' ? https : http;
 		var arg = assign({ method: 'POST' }, parsedUrl, opts);
 
-		var req = fn.request(arg, function (res) {
+		var req = fn.request(arg, function (response) {
+			var statusCode = response.statusCode;
+			var res = response;
+
 			// redirect
-			if (res.statusCode < 400 && res.statusCode >= 300 && res.headers.location) {
+			if (statusCode < 400 && statusCode >= 300 && res.headers.location) {
 				if (++redirectCount > 10) {
 					cb(new Error('Redirected 10 times. Aborting.'), undefined, res);
 					return;
@@ -82,17 +61,19 @@ function sent(url, content, opts, cb) {
 				return;
 			}
 
-			if (res.statusCode < 200 || res.statusCode > 299) {
-				var err = new Error('Couldn\'t connect to ' + url + '.');
-				err.code = res.statusCode;
-				cb(err, undefined, res);
-				return;
-			}
-
 			if (['gzip', 'deflate'].indexOf(res.headers['content-encoding']) !== -1) {
 				var unzip = zlib.createUnzip();
 				res.pipe(unzip);
 				res = unzip;
+			}
+
+			if (statusCode < 200 || statusCode > 299) {
+				read(res, encoding, function (error, data) {
+					var err = error || new Error('Couldn\'t connect to ' + url + '.');
+					err.code = statusCode;
+					cb(err, data, response);
+				});
+				return;
 			}
 
 			// If in proxy mode, simply pipe the response to the proxy.
@@ -101,28 +82,23 @@ function sent(url, content, opts, cb) {
 				return;
 			}
 
-			res.once('error', cb);
-
-			read(res, encoding, cb);
-
+			read(res, encoding, cb, res);
 		}).once('error', cb);
 
-		if (content) {
-			if (content.pipe) {
-				content.pipe(req);
-			} else {
-				req.write(content);
-				req.end();
-			}
+		if (!content) {
+			req.end();
+			return;
+		}
+
+		if (content.pipe) {
+			content.pipe(req);
 		} else {
+			req.write(content);
 			req.end();
 		}
 	};
 
 	send(url, content, opts, cb);
+
 	return proxy;
-}
-
-sent.read = read;
-
-module.exports = sent;
+};
