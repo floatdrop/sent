8a26277a2a02d861ff11ef0e2716a38de242cdcb
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-02 18:20:12 +0500

    Remove test with ReadableStream

--- test.js
+++ test.js
@@ -3,17 +3,8 @@
 'use strict';
 var assert = require('assert');
 var sent = require('./index');
-var from = require('from2-array');
 
 var devNull = 'http://devnull-as-a-service.com/dev/null';
-
-it('should send ReadableStream', function (done) {
-	sent(devNull, from(['Hello']), function (err) {
-		assert.ifError(err);
-		done();
-	});
-});
-
 
 it('should send Buffer', function (done) {
 	sent(devNull, new Buffer('Hello'), function (err) {
