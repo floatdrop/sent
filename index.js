c06890df2b53f44414f2ad97358adc7d38859e5a
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-12-04 21:29:45 +0500

    Add deprecation warning

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 var got = require('got');
 
 module.exports = function (url, content, opts, cb) {
+	console.warn('sent is deprecated. use got instead - https://github.com/sindresorhus/got');
+
 	if (typeof opts === 'function') {
 		// If `cb` has been specified but `opts` has not.
 		cb = opts;
